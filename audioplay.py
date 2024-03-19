c9ea8f0ba93d0bb6de264f151f75bf0657774355
Kaelo <Lentsoekaelo12090@gmail.com>
2024-03-19 09:01:39 +0200

    add

--- audioplay.py
+++ audioplay.py
@@ -3,30 +3,29 @@ import PyPDF2
 from tkinter import filedialog
 from tkinter import Tk
 
-# Initialize Tkinter
+
 root = Tk()
 root.withdraw()
 
-# Ask user to select a PDF file
 book = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
 
-# Check if the file is selected
+
 if book:
     try:
-        # Open the PDF file
+       
         with open(book, "rb") as file:
             pdfreader = PyPDF2.PdfReader(file)
             pages = len(pdfreader.pages)
 
-            # Iterate through each page and extract text
+            
             for num in range(pages):
                 page = pdfreader.pages[num]
                 text = page.extract_text()
 
-                # Initialize the text-to-speech engine
+               
                 player = pyttsx3.init()
 
-                # Say the extracted text
+              
                 player.say(text)
                 player.runAndWait()
     except PyPDF2.utils.PdfReadError as e:
